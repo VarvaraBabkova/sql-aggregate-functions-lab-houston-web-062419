6461e4656d1b1125c01a35e708f999d584783169
Varvara Babkova <varvara.munday@gmail.com>
2019-07-03 16:43:25 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   "select max(gpa) from students;"
@@ -14,5 +14,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  
+
 end
